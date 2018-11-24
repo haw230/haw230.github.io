e58b2be2d850cf7dbebec76a934aeb8e6998622e
Dhruv <17970535+dhruv423@users.noreply.github.com>
2018-11-24 02:14:54 -0500

    federal tax and roi

---
+++ Ependio/js/taxcalc and roi.js
@@ -0,0 +1,32 @@
+
+function ROI(g_invest,c_invest){
+    roi = (g_invest - c_invest) / c_invest
+    return roi
+}
+
+function fedTax(income){
+    if (income <= 46605){ 
+        f_tax = income * 0.15;
+        
+    }
+    else if (income <= 93208) { 
+        f_tax = (income - 46605) * 0.205 + 6991;
+        
+    }
+    else if (income <= 144489) { 
+        f_tax = (income - 93208) * 0.26 + 16544;
+        
+    }
+    else if (income <= 205842) { 
+        f_tax = (income - 144489) * 0.29 + 29877;
+        
+    }
+    else{ 
+        f_tax = (income - 205842) * 0.33 + 47670;
+        
+    }
+
+    return (income - f_tax)
+}
+
+
