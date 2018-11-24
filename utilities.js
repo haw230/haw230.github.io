a2925299624d21f914495ce759063747e2b368bf
haw230 <hancheng.wu22@gmail.com>
2018-11-24 11:57:22 -0500

    Some comments

--- utilities.js
+++ utilities.js
@@ -1,5 +1,19 @@
 // Collection of the functions behind each tool
 
+// Dict of provinces/territories and their tax rates for each bracket
+var rates = {"Newfoundland" : [[0.087, 36926], [0.145, 36926], [0.158, 57998], [0.173, 52740], [0.183, 184590]], 
+            "Prince Edward Island" : [[0.098, 31984], [0.138, 31985], [0.167, 63969]],
+            "Nova Scotia" : [[0.0879, 29590], [0.1495, 29590], [0.1667, 33820], [0.175, 57000], [0.21, 150000]],
+            "New Brunswick" : [[0.0968, 41675], [0.1482, 41676], [0.1652, 52159], [0.1784, 18872], [0.203, 154382]],
+            "Ontario"      :  [[0.0505, 42960], [0.0915, 42963], [0.1116, 64077], [0.1216, 70000], [0.1316, 220000]],
+            "Manitoba"    :  [[0.108, 31843], [0.1275, 36978], [0.174, 68821]],
+            "Saskatchewan" : [[0.105, 45225], [0.125, 83989], [0.145, 129214]],
+            "Alberta"     :  [[0.10, 128145], [0.12, 25628], [0.13, 51258], [0.14, 102516], [0.15, 307547]],
+            "British Columbia " : [[0.0506, 39676], [0.077, 39677], [0.105, 11754], [0.1229, 19523], [0.147, 39370], [0.168, 150000]],
+            "Yukon"        :  [[0.064, 46605], [0.09, 46603], [0.109, 51281], [0.128, 355511], [0.15, 500000]],
+            "Northwest Territories" : [[0.059, 42209], [0.086, 42211], [0.122, 52828], [0.1405, 137248]],
+            "Nunavut"     :  [[0.04, 44437], [0.07, 44437], [0.09, 55614], [0.115, 144488]] };
+
 // Income Tax
 // Purpose
 // incomeTax(income, province) consumes income and province to produce the income after income tax
@@ -33,19 +47,6 @@ function fedTax(income){
 
     return f_tax;
 }
-
-var rates = {"Newfoundland" : [[0.087, 36926], [0.145, 36926], [0.158, 57998], [0.173, 52740], [0.183, 184590]], 
-            "Prince Edward Island" : [[0.098, 31984], [0.138, 31985], [0.167, 63969]],
-            "Nova Scotia" : [[0.0879, 29590], [0.1495, 29590], [0.1667, 33820], [0.175, 57000], [0.21, 150000]],
-            "New Brunswick" : [[0.0968, 41675], [0.1482, 41676], [0.1652, 52159], [0.1784, 18872], [0.203, 154382]],
-            "Ontario"      :  [[0.0505, 42960], [0.0915, 42963], [0.1116, 64077], [0.1216, 70000], [0.1316, 220000]],
-            "Manitoba"    :  [[0.108, 31843], [0.1275, 36978], [0.174, 68821]],
-            "Saskatchewan" : [[0.105, 45225], [0.125, 83989], [0.145, 129214]],
-            "Alberta"     :  [[0.10, 128145], [0.12, 25628], [0.13, 51258], [0.14, 102516], [0.15, 307547]],
-            "British Columbia " : [[0.0506, 39676], [0.077, 39677], [0.105, 11754], [0.1229, 19523], [0.147, 39370], [0.168, 150000]],
-            "Yukon"        :  [[0.064, 46605], [0.09, 46603], [0.109, 51281], [0.128, 355511], [0.15, 500000]],
-            "Northwest Territories" : [[0.059, 42209], [0.086, 42211], [0.122, 52828], [0.1405, 137248]],
-            "Nunavut"     :  [[0.04, 44437], [0.07, 44437], [0.09, 55614], [0.115, 144488]] };
 
 function provincialTax(income, provincialTax){
     var remaining = income;
@@ -89,7 +90,7 @@ function ROI(g_invest,c_invest){
 
 // Compound Calc
 // Purpose
-// compound(principal, interestRate, compoundRate, time) consumes principal, interestRate, compoundRate, time
+// compound(principal, contribution, interestRate, compoundRate, time) consumes principal, contribution, interestRate, compoundRate, time
 // to produce the new amount after being compounded at interestRate at compoundRate for time
 
 // Contract
